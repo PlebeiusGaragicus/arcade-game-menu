3705dba0ce70cdd018c0c65161191c3a2e41fff6
PlebbyG <plebeiusgaragicus@gmail.com>
2023-10-12 13:09:33 -0700

    server settings backend ;)

--- lnarcade/backend/server.py
+++ lnarcade/backend/server.py
@@ -1,8 +1,9 @@
 tips = """
+https://pywebio.readthedocs.io/en/latest/cookbook.html
+
 https://github.com/pywebio/PyWebIO
 
 https://pywebio.readthedocs.io/en/latest/guide.html?highlight=example#layout
-
 """
 
 
@@ -11,19 +12,41 @@ import pywebio
 
 class ArcadeServerPage():
     def __init__(self):
-        self.password = "notinthesauce"
-
-    def page(self):
-        pywebio.output.put_text("Server page")
-        pywebio.output.put_text("Port: %d" % self.server.port)
-        pywebio.output.put_text("Password: %s" % self.server.password)
+        self.password = "asdf"
 
     def start_server(self):
-        pywebio.start_server(self.settings, port=8080, auto_open_webbrowser=False)
+        pywebio.start_server(applications=self.check_password,
+                             host="10.1.10.49",
+                             port=8080,
+                             auto_open_webbrowser=False,
+                            #  remote_access=True
+                             )
 
+    def restart(self, action):
+        print(action)
+        print("RESTART!!")
 
-    def settings(self):
-        port = pywebio.input.input("Enter port number", type=pywebio.input.NUMBER)
+    def backend_page(self):
+        pywebio.output.put_markdown("# Lightning Arcade Settings Backend")
+        pywebio.output.put_markdown(tips)
+        pywebio.output.put_markdown("---")
+
+        # system command button
+        pywebio.output.put_buttons(["restart", "FUCKFUCK"], onclick=self.restart)
+
+        # config file
+        pywebio.pin.put_input(name="port", type=pywebio.input.NUMBER, value=0)
+
+        pywebio.session.hold()
+    
+    def check_password(self):
         password = pywebio.input.input("Enter password", type=pywebio.input.PASSWORD)
-        pywebio.output.put_text("Port number: %d" % port)
-        pywebio.output.put_text("Password: %s" % password)
+        if password != self.password:
+            return
+
+        self.backend_page()
+
+
+if __name__ == "__main__":
+    server = ArcadeServerPage()
+    server.start_server()
